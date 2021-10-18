55a650d667fa6a41682b5a92712628f3cf530a11
apple <apple@apples-macbook-2.local>
2021-10-18 11:15:28 +0500

    Initial Commit

---
+++ NimazApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NimazApp
+//
+//  Created by apple on 18/10/2021.
+//  Copyright © 2021 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
